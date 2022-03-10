94b66b0058c1ba4566bc75e39ecba17cf9e45eac
NewGabrielFroes <gabriel.froes32@gmail.com>
2022-03-10 17:08:43 -0300

    Aprendendo JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = 'Gabriel'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h2>Alterando JSX</h2>
+      <p>Olá, {name}</p>
+      <p>Soma: {sum(2, 2)}</p>
+      <img src={url} alt='Minha Imagem'></img>
     </div>
   );
 }
