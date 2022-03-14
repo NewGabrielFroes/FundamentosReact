f0d546e21e7bf58504f201f1ea1a4e54fd0858c0
NewGabrielFroes <gabriel.froes32@gmail.com>
2022-03-14 15:55:24 -0300

    Trabalhando com renderizacao de listas

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import Evento from './components/Evento';
 import Form from './components/Form';
 import Condicional from './components/Condicional'
+import OutraLista from './components/OutraLista'
 
 function App() {
-  const nome = "Maria"
-  const foto = "https://via.placeholder.com/150"
 
-  return (
-    <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
-    </div>
-  );
+	const meusItens = ['React', 'Vue', 'Angular']
+
+    
+	return (
+	    <div className="App">
+	      <h1>Renderização condicional</h1>
+	      <OutraLista itens={meusItens} />
+	      <OutraLista itens={[]} />
+	    </div>
+  	);
 }
 
 export default App;
